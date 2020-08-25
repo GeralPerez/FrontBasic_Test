d907fa8c85322bb065c4fb8b4d02a51774fb6e67
Geraldin Perez <gplopez@indracompany.com>
2020-08-25 08:22:38 -0500

    aplicacion base fsolo front pendiente servicios

---
+++ src/app/employees/list-employees/list-employees.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+
+export class ListEmployeesComponent implements OnInit {
+id :1;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
